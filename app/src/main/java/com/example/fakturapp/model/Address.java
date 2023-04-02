b8deb23584dfbc7d33b768f502e880e24d496c7e
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2023-04-02 16:02:21 +0200

    create Address #10

---
+++ app/src/main/java/com/example/fakturapp/model/Address.java
@@ -0,0 +1,50 @@
+package com.example.fakturapp.model;
+
+public class Address {
+    private String city;
+    private String street;
+    private String number;
+    private String place;
+
+    public Address() {
+    }
+
+    public Address(String city, String street, String number, String place) {
+        this.city = city;
+        this.street = street;
+        this.number = number;
+        this.place = place;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+}
