d673413f222229cc336e2f9b7cf9ce291c697055
Jakub Kulawik <kuba@MacBook-Air-Jakub.local>
2023-04-29 22:40:50 +0200

    Test after gradle rebuild

--- app/src/main/java/com/fakturapp/MainActivity.java
+++ app/src/main/java/com/fakturapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
+import com.fakturapp.R;
 import com.fakturapp.invoiceGenerator.TemplateGenerator;
 import com.fakturapp.model.Address;
 import com.fakturapp.model.Product;
